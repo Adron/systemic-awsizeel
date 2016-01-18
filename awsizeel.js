0053e1bf94b95ab131fb851ec9bde3626009cc48
Adron Hall (orange) <adron_hall@homedepot.com>
2016-01-18 15:09:16 -0800

    First working code.

--- awsizeel.js
+++ awsizeel.js
@@ -1,3 +1,15 @@
 /**
- * Created by axh6454 on 1/18/16.
+ * Created by Adron Hall on 1/18/16.
  */
+
+var AWS = require('aws-sdk');
+
+AWS.config.update({region: 'us-west-1'});
+
+new AWS.EC2().describeInstances(function (error, data) {
+    if (error) {
+        console.log(error); // an error occurred
+    } else {
+        console.log(data); // request succeeded
+    }
+});
